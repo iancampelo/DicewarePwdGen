d395cf4bf24b87ffeb9712a4bef3c271e8906c63
Ian Campelo <iancampelo@gmail.com>
2015-11-04 18:33:09 -0200

    Add items and adjust controller

--- www/js/app.js
+++ www/js/app.js
@@ -3,17 +3,44 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('diceware', ['ionic', 'firebase'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+.factory('Items',['$firebaseArray', function($firebaseArray){
+  var itemsRef = new Firebase('https://diceware-pwd-gen.firebaseio.com/items');
+  return $firebaseArray(itemsRef);
+}])
+
+.controller('DiceCtrl', function($scope, $ionicListDelegate, $ionicPopup, Items){
+    $scope.items = Items;
+    alert('Dice length'+diceValues.length());
+    alert('Chance gen:' + chance.integer({min: 1, max:6}));
+    var i = roolDice();
+    //Range is quantity of words that will be created
+    $scope.addItem = function(range){
+      var letters=[];
+      for(var i=0;i++;i<range){
+
+      }
+      if(letters.length()>0) {
+        $scope.items.$add({
+          'dice': 'teste'
+        });
+      }
+
+    };
+    $scope.removeItem = function(item){
+      $ionicPopup.confirm({
+        title: 'Remove TODO Item',
+        cssClass: 'ionicModal',
+        template: 'Are you sure about this?'
+      }).then(function(res){
+        if(res) {
+          $scope.items.$remove(item);
+        }
+      });
+    };
 })
+
+function roolDice(){
+  return Math.floor(Math.random() * (6 - 1 + 1)) + 1;
+}
